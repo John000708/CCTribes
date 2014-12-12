bb5204f15a4cf43f78f31e38ae386c34b7e522d9
Martacus <mcpkmart@hotmail.nl>
2014-12-12 20:18:41 +0100

    UUID put in the code. We have to work with this for a while

---
+++ src/main/java/com/tribes/cometcraft/game/GameStart.java
@@ -0,0 +1,20 @@
+package com.tribes.cometcraft.game;
+
+import com.tribes.cometcraft.main.Main;
+
+public class GameStart {
+	
+	Main plugin;
+	
+	public GameStart(Main p){
+		this.plugin = p;
+	}
+	
+	public void startGame(){
+		//TP Playes to their destination
+		//get the mobs ready via a method
+		//Get the timer running on 20-45 minutes
+		//end the grace period after 5 minutes or less
+	}
+
+}
